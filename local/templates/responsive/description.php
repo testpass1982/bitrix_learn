b84e216a9927bfb6cd7ab46fb357898c0693e356
Anatoliy Popov <popov.anatoly@gmail.com>
2019-05-31 12:14:22 +0300

    main page template

--- local/templates/responsive/description.php
+++ local/templates/responsive/description.php
@@ -1,11 +1,8 @@
 <?
-if(!defined("B_PROLOG_INCLUDED") || B_PROLOG_INCLUDED!==true)die();
-
-IncludeTemplateLangFile(__FILE__);
-
-$arTemplate = Array(
-	"NAME"=>GetMessage("RESPONSIVE TEMPLATE"),
-	"DESCRIPTION"=> GetMessage("LEARNING_RESPONSIVE_TEMPLATE")
+$arTemplate = array (
+  'NAME' => 'responsive',
+  'DESCRIPTION' => '',
+  'SORT' => '',
+  'TYPE' => '',
 );
-
 ?>
